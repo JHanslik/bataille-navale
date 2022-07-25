e5ce42d09e64629e6393dc030739413a33eca1b5
JHanslik <julien.hanslik@gmail.com>
2022-07-25 16:02:52 +0200

    Revert "create grille"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import Grille from './components/Grille';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Grille/>
+                <h1>bataille navale</h1>
             </div>
         );
     }
