81fa218da0c05d51f2b9db171817e072d2c56ad3
JHanslik <julien.hanslik@gmail.com>
2022-07-26 11:52:39 +0200

    boat detection function

--- src/components/Grille.js
+++ src/components/Grille.js
@@ -7,10 +7,24 @@ class Grille extends Component {
     this.state = {
       arrayGridX: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       arrayGridY: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+      boat: [
+        [2, "B"],
+        [2, "C"],
+        [2, "D"],
+      ],
     }
   }
+  handleClick = (x, y) => {
+    console.log(x, y)
+  }
 
-  boatRender = (x, y) => {}
+  isFilled = (position) => {
+    console.log(position)
+    return this.state.boat.find((presence) => {
+      console.log(presence)
+      return presence === position
+    })
+  }
 
   render() {
     return (
@@ -19,7 +33,14 @@ class Grille extends Component {
           return (
             <div className="d-flex">
               {this.state.arrayGridY.map((y) => {
-                return <div className={`square`}></div>
+                return (
+                  <div
+                    className={`square ${this.isFilled([x, y]) && "filled"}`}
+                    onClick={() => {
+                      this.handleClick(x, y)
+                    }}
+                  ></div>
+                )
               })}
             </div>
           )
