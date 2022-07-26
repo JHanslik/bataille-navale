28d00180ebe85dde203e97e216827b81c3710760
FloMenus <florent.mns@gmail.com>
2022-07-26 14:58:53 +0200

    done basic first and second game phases

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react"
 import Grille from "./components/Grille"
+import Start from "./components/Start"
+import Placement from "./components/Placement"
 import "./App.css"
 
+
+
 class App extends Component {
+  constructor() {
+  super()
+
+  this.state = {
+    status:"start"
+  }
+}
+
+gameStart = () => {
+  this.setState({
+    status:"placement"
+  })
+}
+placementDone = () => {
+  this.setState ({
+    status:"game"
+  })
+}
   render() {
+    
     return (
-      <div className="container">
-        <Grille />
+      <div>
+        {this.state.status === "start" && <Start startStatus={this.gameStart}/>}
+        {this.state.status === "placement" && <Placement placementDone={this.placementDone}/>}
       </div>
     )
   }
---
+++ src/components/Placement.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import Grille from "./Grille"
+import "../style/placement.css"
+
+
+
+class Start extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Place your boats</h1>
+        </header>
+        <button id="rotate-button" className="mx-auto">Rotate</button>
+        <div className="container"><Grille/></div>
+        <button id="placement-done-button" className="green-button" onClick={this.props.placementDone}>GO</button>
+      </div>
+    )
+  }
+}
+
+export default Start
